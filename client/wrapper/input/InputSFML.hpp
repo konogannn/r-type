16a1bdb6d1615933787de25ae13e9763acea3df4
Nolan Papa <nolan.papa@epitech.eu>
2025-11-24 08:58:53 +0100

    [ADD] Implement InputSFML class for SFML input handling

---
+++ client/wrapper/input/InputSFML.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2025
+** R-type
+** File description:
+** InputSFML - SFML implementation
+*/
+
+#ifndef INPUTSFML_HPP_
+#define INPUTSFML_HPP_
+
+#include "Input.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace rtype {
+
+// Forward declaration
+class WindowSFML;
+
+/**
+ * @brief SFML implementation of IInput interface
+ */
+class InputSFML : public IInput {
+public:
+    /**
+     * @brief Construct a new InputSFML object
+     * @param window Reference to the WindowSFML
+     */
+    explicit InputSFML(WindowSFML& window);
+
+    ~InputSFML() override = default;
+
+    bool isKeyPressed(Key key) const override;
+    bool isMouseButtonPressed(MouseButton button) const override;
+    int getMouseX() const override;
+    int getMouseY() const override;
+
+private:
+    WindowSFML& _window;
+
+    /**
+     * @brief Convert abstract Key to SFML key
+     * @param key Abstract key
+     * @return SFML key code
+     */
+    sf::Keyboard::Key convertKey(Key key) const;
+
+    /**
+     * @brief Convert abstract MouseButton to SFML button
+     * @param button Abstract mouse button
+     * @return SFML mouse button
+     */
+    sf::Mouse::Button convertMouseButton(MouseButton button) const;
+};
+
+} // namespace rtype
+
+#endif /* !INPUTSFML_HPP_ */
