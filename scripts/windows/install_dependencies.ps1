99d4ae6b2d7b099ecc517d9abf17aec4b3563ec7
Nolan Papa <nolan.papa@epitech.eu>
2025-11-24 19:07:23 +0100

    [UPDATE] Add comment for official Chocolatey installation method in install_dependencies.ps1

--- scripts/windows/install_dependencies.ps1
+++ scripts/windows/install_dependencies.ps1
@@ -15,6 +15,7 @@ try {
         Print-Info "Installing Chocolatey..."
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        # Official Chocolatey install method: https://chocolatey.org/install
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
         Print-Ok "Chocolatey installed"
