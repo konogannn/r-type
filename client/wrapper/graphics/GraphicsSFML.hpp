ae0ea1619d8fd1b3abd380071ab4c73a1134e9ce
Nolan Papa <nolan.papa@epitech.eu>
2025-11-24 08:58:18 +0100

    [ADD] Implement SFML graphics rendering class

---
+++ client/wrapper/graphics/GraphicsSFML.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2025
+** R-type
+** File description:
+** GraphicsSFML - SFML implementation
+*/
+
+#ifndef GRAPHICSSFML_HPP_
+#define GRAPHICSSFML_HPP_
+
+#include "Graphics.hpp"
+#include "SpriteSFML.hpp"
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+namespace rtype {
+
+class WindowSFML;
+
+/**
+ * @brief SFML implementation of IGraphics interface
+ */
+class GraphicsSFML : public IGraphics {
+public:
+    /**
+     * @brief Construct a new GraphicsSFML object
+     * @param window Reference to the WindowSFML
+     */
+    explicit GraphicsSFML(WindowSFML& window);
+
+    ~GraphicsSFML() override = default;
+
+    void drawSprite(const ISprite& sprite) override;
+    void drawRectangle(float x, float y, float width, float height,
+                      unsigned char r, unsigned char g, unsigned char b) override;
+    void drawCircle(float x, float y, float radius,
+                   unsigned char r, unsigned char g, unsigned char b) override;
+
+private:
+    WindowSFML& _window;
+};
+
+} // namespace rtype
+
+#endif /* !GRAPHICSSFML_HPP_ */
