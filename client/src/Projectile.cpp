40c8d84e59829eb5e87c6bf34274da9e2f3ca0b7
Nolan Papa <nolan.papa@epitech.eu>
2025-11-26 18:17:57 +0100

    [ADD] Implement Projectile class for handling projectile behavior and rendering

---
+++ client/src/Projectile.cpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2025
+** r-type
+** File description:
+** Projectile
+*/
+
+#include "Projectile.hpp"
+
+#include "../wrapper/graphics/SpriteSFML.hpp"
+
+Projectile::Projectile(const std::string& texturePath, float x, float y)
+    : _x(x), _y(y), _speed(400.0f), _alive(true) {
+    _sprite = std::make_unique<rtype::SpriteSFML>();
+    if (_sprite->loadTexture(texturePath)) {
+        _sprite->setSmooth(false);
+        _sprite->setScale(2.0f, 2.0f);
+        _sprite->setPosition(_x, _y);
+    }
+}
+
+void Projectile::update(float deltaTime) {
+    _x += _speed * deltaTime;
+    if (_sprite) {
+        _sprite->setPosition(_x, _y);
+    }
+
+    if (_x > 850) {
+        _alive = false;
+    }
+}
+
+void Projectile::draw(rtype::IGraphics& graphics) {
+    if (_alive && _sprite) {
+        graphics.drawSprite(*_sprite);
+    }
+}
+
+bool Projectile::isAlive() const { return _alive; }
+
+void Projectile::kill() { _alive = false; }
+
+float Projectile::getX() const { return _x; }
+
+float Projectile::getY() const { return _y; }
+
+float Projectile::getWidth() const { return 32.0f; }
+
+float Projectile::getHeight() const { return 32.0f; }
