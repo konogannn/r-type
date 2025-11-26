94fb0263c0f2473eeabeed22fba074ad3e05aa9b
Nolan Papa <nolan.papa@epitech.eu>
2025-11-26 18:18:57 +0100

    [ADD] Extend Key enum to include function keys F1-F12

--- client/wrapper/input/Input.hpp
+++ client/wrapper/input/Input.hpp
@@ -60,7 +60,19 @@ enum class Key {
     LAlt,
     RControl,
     RShift,
-    RAlt
+    RAlt,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12
 };
 
 // Mouse button codes
