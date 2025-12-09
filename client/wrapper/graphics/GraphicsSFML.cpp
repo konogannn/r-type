1e743fb8408048a78a1aed61798afffcbf3c5609
Nolan Papa <nolan.papa@epitech.eu>
2025-12-09 16:45:02 +0100

    [UPDATE] Add alpha support for drawRectangle and drawText methods in GraphicsSFML

--- client/wrapper/graphics/GraphicsSFML.cpp
+++ client/wrapper/graphics/GraphicsSFML.cpp
@@ -39,6 +39,16 @@ void GraphicsSFML::drawRectangle(float x, float y, float width, float height,
     _window.getSFMLWindow().draw(rectangle);
 }
 
+void GraphicsSFML::drawRectangle(float x, float y, float width, float height,
+                                 unsigned char r, unsigned char g,
+                                 unsigned char b, unsigned char a)
+{
+    sf::RectangleShape rectangle(sf::Vector2f(width, height));
+    rectangle.setPosition(x, y);
+    rectangle.setFillColor(sf::Color(r, g, b, a));
+    _window.getSFMLWindow().draw(rectangle);
+}
+
 void GraphicsSFML::drawCircle(float x, float y, float radius, unsigned char r,
                               unsigned char g, unsigned char b)
 {
@@ -81,6 +91,29 @@ void GraphicsSFML::drawText(const std::string& text, float x, float y,
     sfText.setString(text);
     sfText.setCharacterSize(fontSize);
     sfText.setFillColor(sf::Color(r, g, b));
+    sfText.setPosition(x, y);
+
+    _window.getSFMLWindow().draw(sfText);
+}
+
+void GraphicsSFML::drawText(const std::string& text, float x, float y,
+                            unsigned int fontSize, unsigned char r,
+                            unsigned char g, unsigned char b, unsigned char a,
+                            const std::string& fontPath)
+{
+    sf::Font* font = loadFont(fontPath);
+    if (!font) {
+        std::cerr << "Warning: GraphicsSFML::drawText() - Failed to load font, "
+                     "text not rendered: "
+                  << text << std::endl;
+        return;
+    }
+
+    sf::Text sfText;
+    sfText.setFont(*font);
+    sfText.setString(text);
+    sfText.setCharacterSize(fontSize);
+    sfText.setFillColor(sf::Color(r, g, b, a));
     sfText.setPosition(x, y);
 
     _window.getSFMLWindow().draw(sfText);
