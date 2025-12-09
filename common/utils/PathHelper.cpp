fbf179ec59b45cd6d3c1be9c8b6a28b7adb77170
Nolan Papa <nolan.papa@epitech.eu>
2025-12-09 14:01:46 +0100

    [ADD] Implement PathHelper class for managing asset paths

---
+++ common/utils/PathHelper.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** r-type
+** File description:
+** PathHelper
+*/
+
+#include "PathHelper.hpp"
+
+namespace rtype {
+namespace utils {
+
+std::string PathHelper::_basePath = "";
+bool PathHelper::_initialized = false;
+
+}  // namespace utils
+}  // namespace rtype
