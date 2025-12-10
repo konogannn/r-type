264789f6f7cf6cee92132aeae10b3cd68c499b90
Jules FAYET <jules.fayet@epitech.eu>
2025-12-10 17:57:37 +0100

    [ADD] Create server main.cpp with basic multithreaded server structure

---
+++ server/main.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2025
+** R-TYPE
+** File description:
+** Server main - Multithreaded game server
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+    std::cout << "R-TYPE Server - Main executable" << std::endl;
+    std::cout << "Server initialized (does nothing for now)" << std::endl;
+
+    return 0;
+}
