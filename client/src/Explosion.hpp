877a7533d3619ff1ec4fec89764dcbc7277490d1
Nolan Papa <nolan.papa@epitech.eu>
2025-12-16 14:52:12 +0100

    [FIX]: improve code formatting for draw method in Explosion.hpp

--- client/src/Explosion.hpp
+++ client/src/Explosion.hpp
@@ -23,7 +23,8 @@ class Explosion {
               int totalFrames = 8);
 
     void update(float deltaTime);
-    void draw(IGraphics& graphics, float windowScale, float offsetX, float offsetY);
+    void draw(IGraphics& graphics, float windowScale, float offsetX,
+              float offsetY);
 
     bool isFinished() const;
 
