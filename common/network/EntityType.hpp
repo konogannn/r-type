d6b11d74380537b5525e53aea8f39a9a46d2c2b2
Konogan Pineau <konogan.pineau@epitech.eu>
2026-01-05 17:36:38 +0100

    [FEAT]: update EntityType constants and enhance entityTypeToString mapping

--- common/network/EntityType.hpp
+++ common/network/EntityType.hpp
@@ -14,6 +14,12 @@
  */
 namespace EntityType {
 constexpr uint8_t PLAYER = 1;
-constexpr uint8_t BYDOS = 2;
-constexpr uint8_t MISSILE = 3;
+constexpr uint8_t PLAYER_MISSILE = 2;
+/* 10 -> 40 enemies */
+constexpr uint8_t BASIC = 10; // basic enemy
+constexpr uint8_t BASIC_MISSILE = 11;
+constexpr uint8_t TANK = 12; // tank
+constexpr uint8_t TANK_MISSILE = 13;
+constexpr uint8_t FAST = 14; // fast
+constexpr uint8_t FAST_MISSILE = 15;
 }  // namespace EntityType
