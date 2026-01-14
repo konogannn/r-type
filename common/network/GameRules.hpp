0b768f96165b6cdc38f8c6115c83c9c8b1a57093
Konogan Pineau <konogan.pineau@epitech.eu>
2026-01-14 17:34:00 +0100

    [FEATURE] add GameRules struct for game configuration settings

---
+++ common/network/GameRules.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** r-type
+** File description:
+** GameRules
+*/
+
+#pragma once
+
+#include <cstdint>
+
+/**
+ * @brief Game rules configuration struct
+ * requiredPlayers : Number of players required to start the game
+ * maxRespawn : Maximum number of respawns per player (0 = no respawns)
+ */
+struct GameRules {
+    uint8_t requiredPlayers = 1;
+    uint8_t maxRespawn = 0;
+    bool enablePowerUps = false;
+    bool enableFriendlyFire = false;
+
+    bool operator==(const GameRules& other) const = default;
+};
